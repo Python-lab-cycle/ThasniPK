a7efbb1887234992d8ccd3db0945498dc3e2b036
Thasnipk <76676983+Thasnipk@users.noreply.github.com>
2021-02-22 15:54:53 +0530

    Add files via upload

--- overriding.py
+++ overriding.py
@@ -1,39 +1,34 @@
 class publisher:
-    def __int__(self,pname):
-      self.pname=pname
+    def __init__(self,pname):
+        self.pname=pname
     def display(self):
-      print("name",self.pname)
+        print("name",self.pname)
 class book(publisher):
-    def __init(self,pname,bname,title):
-     self.pname=pname
-     self.bname=bname
-     self.title=title
-
+    def __init__(self,pname,bname,title):
+        self.pname=pname
+        self.bname=bname
+        self.title=title
     def display(self):
-     print("pname",self.pname);
-     print("bname",self.bname);
-     print("title",self.title);
-
-class Python(book):
+        print("pname",self.pname)
+        print("bname",self.bname)
+        print("title",self.title)
+class python(book):
     def __init__(self,pname,bname,title,page,price):
-     self.pname=pname
-     self.bname=bname
-     self.title=title
-     self.page=page
-     self.price=price
-
+        self.pname=pname
+        self.bname=bname
+        self.title=title
+        self.page=page
+        self.price=price
     def display(self):
-     print("pname",self.pname)
-     print("book",self.bname)
-     print("title",self.title)
-     print("pages",self.page)
-     print("price",self.price)
-
+        print("pname",self.pname)
+        print("book",self.bname)
+        print("title",self.title)
+        print("page",self.page)
+        print("price",self.price)
 n=input("enter publisher")
 b=input("enter book")
 t=input("enter title")
 p=int(input("enter page"))
 pr=int(input("enter price"))
-obj=Python(n,b,t,p,pr)
+obj=python(n,b,t,p,pr)
 obj.display()
-    
